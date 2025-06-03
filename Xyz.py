ba572cd96b9d0fb6736ccbdf3d2dfb5876472004
٠\l❼⑧❻ ‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎ <younisxyz18@gmail.com>
2025-06-03 07:10:36 +0500

    Create Xyz.py

---
+++ Xyz.py
@@ -0,0 +1,15 @@
+"""
+    @ code by ---( Younis john )---
+    @ Github : https://github.com/YounisXyz
+    @ WhatsApp : https://wa.me/923194999455
+    @ facebook : https://www.facebook.com/YounisXyz
+    
+"""
+import os, sys,platform
+os.system('xdg-open https://www.facebook.com/YounisXyz')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import old
+elif bit == '32bit':
+    print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
+ 
